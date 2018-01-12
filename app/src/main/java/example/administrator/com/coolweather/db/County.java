592f25032536d786a04c5857cf05c725ba9d00e5
hjf <952567105@qq.com>
2018-01-12 16:29:26 +0800

    加入创建数据库和表的各个配置

---
+++ app/src/main/java/example/administrator/com/coolweather/db/County.java
@@ -0,0 +1,47 @@
+package example.administrator.com.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2018/1/12.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatheId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatheId() {
+        return weatheId;
+    }
+
+    public void setWeatheId(String weatheId) {
+        this.weatheId = weatheId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+}
